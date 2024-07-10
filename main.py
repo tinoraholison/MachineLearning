8e9eb20ec41af90ecccb3df3f30f83bbd43a0693
tinoraholison <thierry.bercytino@gmail.com>
2024-07-10 22:04:26 +0300

    Deuxième Commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import numpy as np
+import pandas as pd
 
 # Exemple d'utilisation de numpy
 matrice = np.random.randn(3, 4)
 print(f"Ma matrice:\n {matrice}")
+
+# Exemple d'utilisation de pandas
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35]}
+print(pd.DataFrame(data))

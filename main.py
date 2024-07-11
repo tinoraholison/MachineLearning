4aa0522db82efa88a858f25855d7c6e374b71e56
tinoraholison <thierry.bercytino@gmail.com>
2024-07-11 21:58:56 +0300

    Troisème Commit

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 # Exemple d'utilisation de numpy
 matrice = np.random.randn(3, 4)
 print(f"Ma matrice:\n {matrice}")
 
 # Exemple d'utilisation de pandas
-data = {'Name': ['Alice', 'Bob', 'Charlie'],
-        'Age': [25, 30, 35]}
-print(pd.DataFrame(data))
+dataPandas = {'Name': ['Alice', 'Bob', 'Charlie'],
+              'Age': [25, 30, 35]}
+print(pd.DataFrame(dataPandas))
+
+# Générateur random data
+data = np.random.rand(10, 10)
+# Création d'un heatmap
+sns.heatmap(data)
+plt.title('Exemple de Heatmap')
+plt.show()
